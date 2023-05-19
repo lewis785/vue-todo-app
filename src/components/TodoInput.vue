0c2d0de8ff274d050251dacea75a8ab664f0a79f
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-05-19 14:08:36 +0100

    ADD TodoInput component

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,9 +1,46 @@
-<script lang="ts">
-export default {
-  name: 'TodoInput'
+<script setup lang="ts">
+import { ref } from 'vue'
+import Checkbox from './Checkbox.vue'
+
+const value = ref('')
+const isChecked = ref(false)
+
+const emit = defineEmits(['addTodo'])
+
+const toggleChecked = () => {
+  isChecked.value = !isChecked.value
+}
+
+const onSubmit = () => {
+  emit('addTodo', value.value)
 }
 </script>
 
 <template>
-  <input type="text" />
+  <form @submit.prevent="onSubmit">
+    <Checkbox @clicked="toggleChecked" :is-checked="isChecked" />
+    <input v-model="value" type="text" placeholder="Create a new todo..." />
+  </form>
 </template>
+
+<style lang="scss" scoped>
+form {
+  display: flex;
+  gap: 1.375rem;
+  padding: 1rem;
+  background: var(--container);
+  font-size: var(--font-regular);
+  border-radius: 5px;
+
+  input {
+    font-size: var(--font-regular);
+    color: var(--text-hover);
+    background: transparent;
+    border: none;
+    outline: none;
+    width: 100%;
+    height: 1.5rem;
+    padding: 0;
+  }
+}
+</style>
