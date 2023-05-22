994516598a728f5e1a4bdc09d42b8d0d0ea585a9
Lewis McNeill <6698050+lewis785@users.noreply.github.com>
2023-05-22 16:19:59 +0100

    ADD TodoListItem component

---
+++ src/components/TodoListItem.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import CrossIcon from '@/icons/CrossIcon.vue'
+import Checkbox from './Checkbox.vue'
+import type { Todo } from '@/types/todo'
+import type { PropType } from 'vue'
+
+const { todo } = defineProps({
+  todo: {
+    type: Object as PropType<Todo>,
+    required: true
+  }
+})
+
+const emit = defineEmits<{
+  (event: 'delete', todo: Todo): void
+  (event: 'update', todo: Todo): void
+}>()
+</script>
+
+<template>
+  <article class="item" :data-completed="todo.completed">
+    <Checkbox
+      :is-checked="todo.completed"
+      @clicked="emit('update', { ...todo, completed: !todo.completed })"
+    />
+    <p>{{ todo.value }}</p>
+    <CrossIcon class="remove-icon" @click="emit('delete', todo)" />
+  </article>
+</template>
+
+<style lang="scss" scoped>
+.item {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  border-bottom: 1px solid var(--border);
+  padding: 1rem 1.375rem;
+
+  p {
+    width: 100%;
+  }
+
+  &[data-completed='true'] {
+    p {
+      text-decoration: line-through;
+      color: var(--text-disabled);
+    }
+  }
+}
+</style>
